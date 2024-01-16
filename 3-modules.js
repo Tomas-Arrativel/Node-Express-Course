7eca283b4c3b6cda924ecec1aaa59d36ac02c5a5
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-16 19:32:01 -0300

    Alternative sintax

--- 3-modules.js
+++ 3-modules.js
@@ -2,6 +2,10 @@
 const names = require('./4-names');
 const sayHi = require('./5-utils');
 
+const data = require('./6-alternative-flavor');
+
 sayHi('Tomás');
 sayHi(names.john);
 sayHi(names.peter);
+
+console.log(data);
---
+++ 6-alternative-flavor.js
@@ -0,0 +1,8 @@
+// Alternative sintax
+
+module.exports.item = ['item1', 'item2'];
+const person = {
+  name: 'Tomás',
+};
+
+module.exports.singlePerson = person;
