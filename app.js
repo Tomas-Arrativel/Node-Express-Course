7d0815c4d9859f69e2a5e2a7c9d8b2bab3db0c11
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-19 19:09:23 -0300

    passing props from function emit

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter();
 
-customEmitter.on('response', () => {
-  console.log(`Data recieved`);
+customEmitter.on('response', (name, id) => {
+  console.log(`Data recieved by ${name} with an id of ${id}`);
 });
 
-customEmitter.emit('response');
+customEmitter.on('response', () => {
+  console.log(`Other logic here...`);
+});
+
+customEmitter.emit('response', 'John', 34);
