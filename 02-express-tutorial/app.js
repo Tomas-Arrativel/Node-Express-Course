9a61188e60c7f3c7af384587271522b9c2abb290
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-22 19:04:22 -0300

    http basics in the project

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('User hit the server');
+  res.end('<h1>Home page</h1>');
+});
+
+server.listen(5000);
