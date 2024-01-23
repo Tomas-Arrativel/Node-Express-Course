8d4a196a0cbdd994bf58fba436a93aa9c472a4ef
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-23 11:53:06 -0300

    how to check if user is going to an url or to another

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  // console.log(req.method);
-  console.log(req.url);
-
-  res.writeHead(200, { 'content-type': 'text/html' });
-  res.write('<h1>Home page</h1>');
-  res.end();
+  const url = req.url;
+  //Home page
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Home page</h1>');
+    res.end();
+  } //About page
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>About me</h1>');
+    res.end();
+  } //404
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>Page not found</h1>');
+    res.end();
+  }
 });
 
 server.listen(5000);
