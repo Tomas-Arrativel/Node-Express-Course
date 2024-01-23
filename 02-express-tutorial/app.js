064c0137151d5148ba4cf72dda6b3d92559bafdc
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-23 18:24:26 -0300

    html file rendered in node server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,11 +1,15 @@
 const http = require('http');
+const { readFileSync } = require('fs');
+
+//Get all files
+const homePage = readFileSync('./index.html');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   //Home page
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>Home page</h1>');
+    res.write(homePage);
     res.end();
   } //About page
   else if (url === '/about') {
