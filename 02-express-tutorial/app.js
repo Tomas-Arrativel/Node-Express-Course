bd943cd0d5f9081839926dbaa365522d1604655a
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-24 09:29:19 -0300

    Render an app without the get method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,9 +7,12 @@ const app = express();
 // Static folders are files that server doesn't has to change
 app.use(express.static('./navbar-app'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname + './navbar-app/index.html'));
-});
+// This get is not needed
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname + './navbar-app/index.html'));
+//   adding to static assets
+//   SSR (Server Side Rendering)
+// });
 
 app.all('*', (req, res) => {
   res.status(404).send('Resource not found');
