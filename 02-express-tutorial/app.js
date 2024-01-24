11d5762b0a51ea7c79a592119e5986f583b9dcf8
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-24 10:44:31 -0300

    Query string example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -28,8 +28,26 @@ app.get('/api/products/:productID', (req, res) => {
 });
 
 app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params);
   res.send('Hello world');
+});
+
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1)
+    return res.status(200).send('No product matched your search');
+
+  res.status(200).json(sortedProducts);
 });
 
 app.all('*', (req, res) => {
