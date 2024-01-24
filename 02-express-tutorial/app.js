b3b7f568c281abc8bf48dc036851418c6e68b3de
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-24 09:44:17 -0300

    json basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// It is used normally with the public folder
-// Static folders are files that server doesn't has to change
-app.use(express.static('./navbar-app'));
-
-// This get is not needed
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname + './navbar-app/index.html'));
-//   adding to static assets
-//   SSR (Server Side Rendering)
-// });
-
-app.all('*', (req, res) => {
-  res.status(404).send('Resource not found');
+app.get('/', (req, res) => {
+  res.json([{ name: 'John' }, { name: 'Tomas' }]);
 });
 
 const port = 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
