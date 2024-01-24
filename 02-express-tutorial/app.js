b157e6ce10e4591b0919219e893f26f8972ae8b0
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-24 16:57:41 -0300

    Using a middleware logger for the GET home page method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,59 +1,21 @@
 const express = require('express');
-const { products } = require('./data');
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home page</h1><a href="/api/products">Products</a>');
+// req => middleware => res
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const date = new Date().getFullYear();
+  console.log(method, url, date);
+  next();
+};
+
+app.get('/', logger, (req, res) => {
+  res.send('Home');
 });
 
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.status(200).json(newProducts);
-});
-
-app.get('/api/products/:productID', (req, res) => {
-  const { productID } = req.params;
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID),
-  );
-
-  if (!singleProduct) return res.status(404).send('<h1>Product not found</h1>');
-
-  return res.json(singleProduct);
-});
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  res.send('Hello world');
-});
-
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1)
-    // We cant send 2 responses per request
-    // res.status(200).send('No product matched your search');
-    return res.status(200).send('No product matched your search');
-
-  res.status(200).json(sortedProducts);
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Page not found</h1>');
+app.get('/about', (req, res) => {
+  res.send('About');
 });
 
 const port = 5000;
