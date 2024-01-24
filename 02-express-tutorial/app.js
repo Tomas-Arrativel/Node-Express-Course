6372fc90cd270a84da04e928b53bca5fcebd343b
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-24 17:02:47 -0300

    making the logger function a module

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const app = express();
 
+const logger = require('./logger');
+
 // req => middleware => res
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const date = new Date().getFullYear();
-  console.log(method, url, date);
-  next();
-};
 
 app.get('/', logger, (req, res) => {
   res.send('Home');
