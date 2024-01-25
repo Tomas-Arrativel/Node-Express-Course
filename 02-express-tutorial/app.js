a3ec944b16e16d045edb16ae8fc4fc6316539fca
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-25 08:21:00 -0300

    POST method form example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,8 +3,20 @@ const app = express();
 
 let { people } = require('./data');
 
+//  Static folder
+app.use(express.static('./methods-public'));
+//  Parse Form Data
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post('/login', (req, res) => {
+  const { name } = req.body;
+  if (name) return res.status(200).send(`<h1>Welcome ${name}</h1>`);
+
+  res.status(401).send('<h1>You need to provide your information</h1>');
 });
 
 const port = 5000;
