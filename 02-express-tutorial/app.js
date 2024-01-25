10c2e118ded0227a7766f5e05edf9f46c2933e44
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-25 10:34:57 -0300

    Making a router of auth

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const peopleRouter = require('./routes/people');
+const authRouter = require('./routes/auth');
 
 //  Static folder
 app.use(express.static('./methods-public'));
@@ -11,13 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/api/people', peopleRouter);
-
-app.post('/login', (req, res) => {
-  const { name } = req.body;
-  if (name) return res.status(200).send(`<h1>Welcome ${name}</h1>`);
-
-  res.status(401).send('<h1>You need to provide your information</h1>');
-});
+app.use('/login', authRouter);
 
 const port = 5000;
 app.listen(port, () => {
