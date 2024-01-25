61fbffb36d1d758e3a133aa6a3c867a20c300443
Tomás Arrativel <arrativeltomas@gmail.com>
2024-01-25 10:08:58 -0300

    DELETE method with postman example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -47,16 +47,28 @@ app.put('/api/people/:id', (req, res) => {
   const { name } = req.body;
 
   const person = people.find((person) => person.id === Number(id));
-  if (!person) {
+  if (!person)
     return res
       .status(404)
       .json({ success: false, msg: `No person with id ${id}` });
-  }
 
   const newPeople = people.map((person) => {
     if (person.id === Number(id)) person.name = name;
     return person;
   });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person)
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${id}` });
+
+  const newPeople = people.filter((person) => person.id !== Number(id));
   res.status(200).json({ success: true, data: newPeople });
 });
 
